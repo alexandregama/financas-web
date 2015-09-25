5ab2795fcf38d0ad638878c15577193c7dfdd5b1
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-24 21:59:55 -0300

    Permite fazer o Crud de Contas utilizando @Stateless do EJB e @Inject e @Named do CDI

--- src/br/com/caelum/financas/dao/ContaDao.java
+++ src/br/com/caelum/financas/dao/ContaDao.java
@@ -2,34 +2,42 @@ package br.com.caelum.financas.dao;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.financas.modelo.Conta;
 
+@Stateless
 public class ContaDao {
 
-	EntityManager manager;
+	@PersistenceContext
+	private EntityManager manager;
 
 	public void adiciona(Conta conta) {
-		this.manager.persist(conta);
+		manager.persist(conta);
 	}
 
 	public Conta busca(Integer id) {
-		return this.manager.find(Conta.class, id);
+		return manager.find(Conta.class, id);
 	}
 
+	public void atualiza(Conta conta) {
+		manager.merge(conta);
+	}
+	
 	public List<Conta> lista() {
-		return this.manager.createQuery("select c from Conta c", Conta.class)
-				.getResultList();
+		String jpql = "select c from Conta c";
+		TypedQuery<Conta> query = manager.createQuery(jpql, Conta.class);
+
+		return query.getResultList();
 	}
 
 	public void remove(Conta conta) {
 		Conta contaParaRemover = this.manager.find(Conta.class, conta.getId());
-		this.manager.remove(contaParaRemover);
+
+		manager.remove(contaParaRemover);
 	}
 
 }
-
-
-
-
--- src/br/com/caelum/financas/mb/ContasBean.java
+++ src/br/com/caelum/financas/mb/ContasBean.java
@@ -2,19 +2,62 @@ package br.com.caelum.financas.mb;
 
 import java.io.Serializable;
 import java.util.List;
-import br.com.caelum.financas.modelo.Conta;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.caelum.financas.dao.ContaDao;
+import br.com.caelum.financas.modelo.Conta;
 
 @Named
 @ViewScoped
 public class ContasBean implements Serializable {
-    
-    private static final long serialVersionUID = 1L;
+
+	private static final long serialVersionUID = 1L;
+
+	private ContaDao contaDao;
 
 	private Conta conta = new Conta();
+	
 	private List<Conta> contas;
+
+	@Inject
+	public ContasBean(ContaDao contaDao) {
+		this.contaDao = contaDao;
+	}
+	
+	@Deprecated //default constructor for CDI 
+	ContasBean() {
+	}
+
+	public void grava() {
+		if (conta.getId() == null) {
+			contaDao.adiciona(conta);
+		} else {
+			contaDao.atualiza(conta);
+		}
+
+		limpaFormularioDoJSF();
+	}
+
+	public List<Conta> getContas() {
+		if (contas == null) {
+			contas = contaDao.lista();
+		}
+		return contas;
+	}
+
+	public void remove() {
+		contaDao.remove(conta);
+
+		limpaFormularioDoJSF();
+	}
+
+	private void limpaFormularioDoJSF() {
+		conta = new Conta();
+		contas = null; 
+	}
 
 	public Conta getConta() {
 		return conta;
@@ -22,31 +65,5 @@ public class ContasBean implements Serializable {
 
 	public void setConta(Conta conta) {
 		this.conta = conta;
-	}
-
-	public void grava() {
-		System.out.println("Gravando a conta");
-
-		limpaFormularioDoJSF();
-	}
-
-	public List<Conta> getContas() {
-		System.out.println("Listando as contas");
-
-		return contas;
-	}
-
-	public void remove() {
-		System.out.println("Removendo a conta");
-
-		limpaFormularioDoJSF();
-	}
-
-	/**
-	 * Esse metodo apenas limpa o formulario da forma com que o JSF espera.
-	 * Invoque-o no momento em que precisar do formulario vazio.
-	 */
-	private void limpaFormularioDoJSF() {
-		this.conta = new Conta();
 	}
 }
