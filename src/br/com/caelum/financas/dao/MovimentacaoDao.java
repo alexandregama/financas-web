dfea93a5ece7de7d596c5c497bda0680d0ee7270
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-30 21:34:48 -0300

    Adiciona validacao checked para nao causar o Rollback

--- src/br/com/caelum/financas/dao/MovimentacaoDao.java
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -16,12 +16,16 @@ public class MovimentacaoDao {
 	@PersistenceContext
 	private EntityManager manager;
 
-	public void adiciona(Movimentacao movimentacao) {
+	public void adiciona(Movimentacao movimentacao) throws Exception {
 		
 		manager.persist(movimentacao);
 		
 		if (movimentacao.getValor().compareTo(BigDecimal.ZERO) == -1) {
 			throw new RuntimeException("O valor não pode ser negativo - sua transação sofreu Rollback");
+		}
+		
+		if (movimentacao.getValor().compareTo(new BigDecimal(1000)) == 1) {
+			throw new Exception("O valor não pode ser maior que 1000 - mas sua transação foi comitada");
 		}
 	}
 
