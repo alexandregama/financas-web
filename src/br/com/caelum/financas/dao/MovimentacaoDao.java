97824492b8180879e9eb925c0b77abb3c71ce623
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-30 21:41:19 -0300

    Adiciona validacao customizada Checked para nao ocorrer o rollback

--- src/br/com/caelum/financas/dao/MovimentacaoDao.java
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -8,7 +8,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import br.com.caelum.financas.exception.ValorMuitoAltoException;
 import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
 
 @Stateless
 public class MovimentacaoDao {
@@ -24,8 +26,12 @@ public class MovimentacaoDao {
 			throw new RuntimeException("O valor não pode ser negativo - sua transação sofreu Rollback");
 		}
 		
-		if (movimentacao.getValor().compareTo(new BigDecimal(1000)) == 1) {
-			throw new Exception("O valor não pode ser maior que 1000 - mas sua transação foi comitada");
+		if (movimentacao.getValor().compareTo(new BigDecimal(1000)) == 1 && movimentacao.isEntrada()) {
+			throw new Exception("O valor não pode ser maior que 1000 e de Entrada - mas sua transação foi comitada");
+		}
+		
+		if (movimentacao.getValor().compareTo(new BigDecimal(3000)) == 1 && movimentacao.isSaida()) {
+			throw new ValorMuitoAltoException("O valor nao pode ser acima de 3000 e de Saida - mas sua transacao foi comitada");
 		}
 	}
 
