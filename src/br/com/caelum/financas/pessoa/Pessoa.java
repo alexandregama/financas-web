45d65336b7df639ce8497e14e6513a5838f5482e
alexandregama <alexandre.gama.lima@gmail.com>
2015-10-08 18:27:04 -0300

    Adiciona a classe Pessoa para usar herança

---
+++ src/br/com/caelum/financas/pessoa/Pessoa.java
@@ -0,0 +1,25 @@
+package br.com.caelum.financas.pessoa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
