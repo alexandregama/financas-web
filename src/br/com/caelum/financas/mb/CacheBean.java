537eeeddd2f5b6787e5a3b10f7ec1b5e1658e135
alexandregama <alexandre.gama.lima@gmail.com>
2015-10-14 18:22:17 -0300

    Adiciona pesquisa de Conta para testar Cache de Primeiro nivel

--- src/br/com/caelum/financas/mb/CacheBean.java
+++ src/br/com/caelum/financas/mb/CacheBean.java
@@ -1,29 +1,37 @@
 package br.com.caelum.financas.mb;
 
-import br.com.caelum.financas.modelo.Conta;
-
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.financas.modelo.Conta;
 
 @Named
 @RequestScoped
 public class CacheBean {
-	
+
 	private Integer id;
 	private Conta conta;
+
+	@PersistenceContext
+	private EntityManager manager;
 	
 	public void pesquisar() {
-			System.out.println("Testando cache de primeiro nivel");
+		System.out.println("Testando cache de primeiro nivel");
+		conta = manager.find(Conta.class, id);
+		conta = manager.find(Conta.class, id);
+		conta = manager.find(Conta.class, id);
 	}
 
 	public Integer getId() {
 		return id;
 	}
-	
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public Conta getConta() {
 		return conta;
 	}
