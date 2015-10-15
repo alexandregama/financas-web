d807aaf3631efdc4dd27d8af47cd992955191b1e
alexandregama <alexandre.gama.lima@gmail.com>
2015-10-15 18:31:57 -0300

    Adiciona as estatisticas para o Cacne das entidades Conta e Movimentacao

--- src/br/com/caelum/financas/mb/EstatisticasBean.java
+++ src/br/com/caelum/financas/mb/EstatisticasBean.java
@@ -6,6 +6,7 @@ import javax.inject.Named;
 import javax.persistence.EntityManager;
 
 import org.hibernate.Session;
+import org.hibernate.stat.EntityStatistics;
 import org.hibernate.stat.Statistics;
 
 @Named
@@ -17,14 +18,29 @@ public class EstatisticasBean {
 	
 	private Statistics estatisticas;
 	
+	private EntityStatistics movimentacaoEntityStatistics;
+	
+	private EntityStatistics contaEntityStatistics;
+	
 	public void gera() {
 		Session session = manager.unwrap(Session.class);
 		estatisticas = session.getSessionFactory().getStatistics();
+		movimentacaoEntityStatistics = estatisticas.getEntityStatistics("br.com.caelum.financas.modelo.Movimentacao");
+		contaEntityStatistics = estatisticas.getEntityStatistics("br.com.caelum.financas.modelo.Conta");
+		
 		System.out.println("Gerando estatisitcas");
 	}
 
 	public Statistics getEstatisticas() {
 		return estatisticas;
+	}
+
+	public EntityStatistics getMovimentacaoEntityStatistics() {
+		return movimentacaoEntityStatistics;
+	}
+	
+	public EntityStatistics getContaEntityStatistics() {
+		return contaEntityStatistics;
 	}
 	
 }
